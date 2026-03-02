020d6e6b9e2c6ef1410a9857e94f272681509d49
Ananthakrishnan KV <149510501+anand-KV@users.noreply.github.com>
2026-03-02 13:21:31 +0530

    change

--- backend/server.js
+++ backend/server.js
@@ -18,10 +18,14 @@ app.use("/api/users", userRoutes);
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/lmDB1")
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
 app.use("/api", authRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () =>
+  console.log(`Server running on port ${PORT}`)
+);
